4381e2b4a9661dbabd6b475e4219e0677fbf61cb
Christian Köhler <christian.koehler@gwdg.de>
2017-05-19 13:05:53 +0200

    add epsilon runtime for batchjobs

---
+++ dist/batch/src/main/java/de/swz/dist/batch/jobscriptGenerator.java
@@ -0,0 +1,66 @@
+package de.swz.dist.batch;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.net.URL;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.eclipse.epsilon.examples.standalone.EpsilonStandaloneExample;
+import org.eclipse.epsilon.eol.IEolExecutableModule;
+import org.eclipse.epsilon.eol.models.IModel;
+import org.eclipse.epsilon.egl.EglTemplateFactory;
+import org.eclipse.epsilon.egl.EglTemplateFactoryModuleAdapter;
+
+
+public class jobscriptGenerator extends EpsilonStandaloneExample {
+	String inputModelPath;
+	String inputMetamodelPath;
+	String templatePath;
+	//String outputFilePath;
+
+	public String getResult() {
+		return result.toString();
+	}
+
+	@Override
+	public IEolExecutableModule createModule() {
+		//System.out.println("...createModule");
+		EglTemplateFactory factory = new EglTemplateFactory();
+		return new EglTemplateFactoryModuleAdapter(factory);
+	}
+
+	@Override
+	public String getSource() throws Exception {
+		//System.out.println("...getSource");
+		return templatePath;
+	}
+
+	@Override
+	public List<IModel> getModels() throws Exception {
+		//System.out.println("...getModels");
+		List<IModel> models = new ArrayList<IModel>();
+		models.add(createEmfModel("jobscript", inputModelPath, inputMetamodelPath, true, false));
+		return models; 
+	}
+
+	@Override
+	public URI getFileURI(String fileName) throws URISyntaxException {
+		//System.out.println("...getFileURI(" + fileName + ")");
+		URI uri = getClass().getResource("/" + fileName).toURI();
+		//System.out.println("-> " + uri.toString());
+		return uri;
+	}
+
+	public String getSourceFromClasspath (String name) {
+		URL sourceURL = this.getClass().getResource("/" + name);
+		return sourceURL.getPath();
+	}
+
+	public jobscriptGenerator(String inputModelPath, String inputMetamodelPath, String templatePath) {
+		//System.out.println("This is jobscriptGenerator.");
+		this.inputModelPath = inputModelPath;
+		this.inputMetamodelPath = inputMetamodelPath;
+		this.templatePath = templatePath;
+	}
+}

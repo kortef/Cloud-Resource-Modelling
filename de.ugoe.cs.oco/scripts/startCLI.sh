d481d294f4156a1603fb1bc7513d2bb1b5b42f19
fglaser <fglaser3@aol.com>
2017-05-19 10:09:15 +0200

    Added script to start CLI with correct CLASSPATH

---
+++ de.ugoe.cs.oco/scripts/startCLI.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+java -classpath ../bundles/de.ugoe.cs.oco.thirdparty/libs/java-utils-0.1.3-SNAPSHOT.jar:../bundles/de.ugoe.cs.oco.cli/target/de.ugoe.cs.oco.cli-1.0.0-SNAPSHOT.jar de.ugoe.cs.oco.cli.Runner
+

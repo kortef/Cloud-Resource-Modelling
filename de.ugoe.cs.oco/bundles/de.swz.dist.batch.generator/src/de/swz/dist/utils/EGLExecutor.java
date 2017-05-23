7b0bd840218d9672fa9ce89e896b1309ea95a62d
Christian Köhler <christian.koehler@gwdg.de>
2017-05-23 10:53:36 +0200

    add class EGLExecutor

---
+++ de.ugoe.cs.oco/bundles/de.swz.dist.batch.generator/src/de/swz/dist/utils/EGLExecutor.java
@@ -0,0 +1,28 @@
+package de.swz.dist.utils;
+
+import java.util.List;
+
+import org.eclipse.epsilon.eol.IEolExecutableModule;
+import org.eclipse.epsilon.eol.models.IModel;
+
+public class EGLExecutor extends EpsilonExecutor {
+
+	@Override
+	public IEolExecutableModule createModule() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getSource() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<IModel> getModels() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

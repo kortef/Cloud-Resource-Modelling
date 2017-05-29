f2666a185094bf245e77fc31e7767e3315a46b78
Christian Köhler <christian.koehler@gwdg.de>
2017-05-29 18:03:43 +0200

    add java-utils to test project

--- dist/batch/src/main/java/de/swz/dist/batch/App.java
+++ dist/batch/src/main/java/de/swz/dist/batch/App.java
@@ -1,6 +1,7 @@
 package de.swz.dist.batch;
 
 import de.swz.dist.batch.jobscriptGenerator;
+import de.ugoe.cs.util.console.TextConsole;
 
 /**
  * Hello world!
@@ -10,6 +11,8 @@ public class App
 {
     public static void main( String[] args )
     {
+    	TextConsole t = new TextConsole();
+    	t.outputMsg("Hello from de.ugoe.cs.util.console.TextConsole !\n");
         jobscriptGenerator j = new jobscriptGenerator("amroc_testrun.model", "jobscript.ecore", "jobscript.egl");
         //j.createModule();
         try {

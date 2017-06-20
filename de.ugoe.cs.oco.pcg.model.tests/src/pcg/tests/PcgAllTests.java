1d879a2a50af3acce534a0bf2d91e1cef8644b16
rockodell <rockodell@work>
2017-06-20 14:50:07 +0200

    Change-Id: I09ee66fd0f40db3c4449ebba89d94916a2f8dbee

---
+++ de.ugoe.cs.oco.pcg.model.tests/src/pcg/tests/PcgAllTests.java
@@ -0,0 +1,46 @@
+/**
+ */
+package pcg.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import junit.textui.TestRunner;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test suite for the '<em><b>Pcg</b></em>' model.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class PcgAllTests extends TestSuite {
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(suite());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static Test suite() {
+		TestSuite suite = new PcgAllTests("Pcg Tests");
+		return suite;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public PcgAllTests(String name) {
+		super(name);
+	}
+
+} //PcgAllTests

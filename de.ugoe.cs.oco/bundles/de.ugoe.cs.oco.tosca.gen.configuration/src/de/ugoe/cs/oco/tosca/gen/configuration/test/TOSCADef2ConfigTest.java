0345179039400e8be72d787d19133578ea7e87bb
Fabian Korte <fabian.korte@cs.uni-goettingen.de>
2018-09-10 22:34:23 +0200

    Added TOSCA Definition to TOSCA Configuration Transformation

---
+++ de.ugoe.cs.oco/bundles/de.ugoe.cs.oco.tosca.gen.configuration/src/de/ugoe/cs/oco/tosca/gen/configuration/test/TOSCADef2ConfigTest.java
@@ -0,0 +1,20 @@
+package de.ugoe.cs.oco.tosca.gen.configuration.test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Test;
+
+import de.ugoe.cs.oco.tosca.gen.configuration.TOSCADef2Config;
+
+public class TOSCADef2ConfigTest {
+	
+	@Test
+	public void testGenerateConfig() {
+		Path toscaDef = Paths.get("/home/fglaser/de.ugoe.cs.oco.usecases/sugercrm_original/Definitions/SugarCRM-Interop-Definitions.tosca");
+	    Path generatedEcore = Paths.get("/home/fglaser/de.ugoe.cs.oco.usecases/sugercrm_original/Definitions/SugarCRM-Interop-Definitions-migrated.tosca");
+
+		TOSCADef2Config.generateConfiguration(toscaDef, generatedEcore);
+	}
+
+}

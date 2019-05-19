25be54e944c398a4fc43b279e3dfb990e154e517
kortef <fbnkorte@gmail.com>
2019-05-19 23:56:41 +0200

    Update tocci and test Runner

--- de.ugoe.cs.as/bundles/de.ugoe.cs.as.cli/src/de/ugoe/cs/as/cli/Runner.java
+++ de.ugoe.cs.as/bundles/de.ugoe.cs.as.cli/src/de/ugoe/cs/as/cli/Runner.java
@@ -10,12 +10,13 @@ import de.ugoe.cs.util.console.TextConsole;
 public class Runner {
 
 	public static void main(String[] args) {
-		CommandExecuter.getInstance().addCommandPackage("de.ugoe.cs.oco.cli");
-		CommandExecuter.getInstance().addCommandPackage("de.ugoe.cs.oco.tosca.instantiator.commands");
-		CommandExecuter.getInstance().addCommandPackage("de.ugoe.cs.oco.tosca.preprocessor.commands");
-		CommandExecuter.getInstance().addCommandPackage("de.ugoe.cs.oco.tosca.yamlgenerator.commands");
-		CommandExecuter.getInstance().addCommandPackage("de.ugoe.cs.oco.tosca.yamlparser.commands");
-		CommandExecuter.getInstance().addCommandPackage("de.ugoe.cs.oco.tosca2occi.commands");
+		CommandExecuter.getInstance().addCommandPackage("de.ugoe.cs.as.cli");
+		//CommandExecuter.getInstance().addCommandPackage("de.ugoe.cs.as.tosca.instantiator.commands");
+		//CommandExecuter.getInstance().addCommandPackage("de.ugoe.cs.as.tosca.preprocessor.commands");
+		//CommandExecuter.getInstance().addCommandPackage("de.ugoe.cs.as.tosca.yamlgenerator.commands");
+		//CommandExecuter.getInstance().addCommandPackage("de.ugoe.cs.as.tosca.yamlparser.commands");
+		CommandExecuter.getInstance().addCommandPackage("de.ugoe.cs.as.tosca2occi.commands");
+		CommandExecuter.getInstance().addCommandPackage("de.ugoe.cs.as.usecases.commands");
 		
 		EcorePlugin.ExtensionProcessor.process(null);
 	
